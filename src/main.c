3bce6ab8497eda80e9ee57b988089c0f258c564d
William QM <william.quintas@gmail.com>
2018-03-07 17:01:17 -0300

    now reading the expression

--- src/main.c
+++ src/main.c
@@ -1,9 +1,13 @@
 /* Somador de numeros
- *
-*/
+ * Nome: William Quintas de Melo - RA: 188684
+ * EA876 - Introdução a Software de Sistema - Turma A
+ * Professor Tiago Tavares
+ */
 
+#include <stdio.h>
 
 int main() {
-
+  char in[30];
+  scanf("%[A-Za-z0-9 .!]s", in);
   return 0;
 }
